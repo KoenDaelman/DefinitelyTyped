6450355f23047c39f7d00970a5bc3808b11aee52
Ewout Van Gossum <ewout.vangossum@qmino.com>
2016-05-17 09:48:48 +0200

    Makes Snaplines options any type.

--- rappid/rappid.d.ts
+++ rappid/rappid.d.ts
@@ -9,79 +9,79 @@
 declare module joint {
     module ui {
         interface Handle {
-            name : string;
-            position : string;
+            name: string;
+            position: string;
             icon: string;
         }
-		
-		class Snaplines extends Backbone.Model {
-            constructor(opt: {
-                paper: joint.dia.Paper;
-            });
+
+        class Snaplines extends Backbone.Model {
+            constructor(opt: any);
 
             startListening(): void;
         }
 
 
         class SelectionView extends Backbone.Model {
-            paper:joint.dia.Paper;
-            graph:joint.dia.Graph;
-            model:Backbone.Collection<joint.dia.Cell>;
+            paper: joint.dia.Paper;
+            graph: joint.dia.Graph;
+            model: Backbone.Collection<joint.dia.Cell>;
 
-            constructor(opt:{
-                paper : joint.dia.Paper;
-                graph  : joint.dia.Graph;
-                model : Backbone.Collection<joint.dia.Cell>
+            constructor(opt: {
+                paper: joint.dia.Paper;
+                graph: joint.dia.Graph;
+                model: Backbone.Collection<joint.dia.Cell>
             });
 
-            createSelectionBox(cellView:joint.dia.CellView):void;
+            createSelectionBox(cellView: joint.dia.CellView): void;
 
-            destroySelectionBox(cellView:joint.dia.CellView):void;
+            destroySelectionBox(cellView: joint.dia.CellView): void;
 
-            startSelecting(evt:any):void;
+            startSelecting(evt: any): void;
 
-            cancelSelection():void;
+            cancelSelection(): void;
 
-            addHandle(handle:Handle):void;
+            addHandle(handle: Handle): void;
 
-            removeHandle(name:string):void;
+            removeHandle(name: string): void;
 
-            changeHandle(name:string, handle:Handle):void;
+            changeHandle(name: string, handle: Handle): void;
         }
 
         class PaperScroller extends Backbone.View<Backbone.Model> {
 
-            constructor(opt?:{
-                paper : joint.dia.Paper;
+            constructor(opt?: {
+                paper: joint.dia.Paper;
                 autoResizePaper ?: boolean
             });
 
-            startPanning(evt:any):void;
-            pan(evt:any):void;
-            stopPanning(evt:any):void;
+            startPanning(evt: any): void;
 
-            center():void;
-            center(x:number, y:number):void;
+            pan(evt: any): void;
 
-            centerContent():void;
+            stopPanning(evt: any): void;
 
-            zoom(value:number, opt?:{
-                max:number;
-                min:number;
-                grid:any;
-                absolute:boolean;
-            }):void;
+            center(): void;
+            center(x: number, y: number): void;
 
-            zoomToFit(opt?:{
-                padding?:number;
-                preserveAspectRatio?:boolean;
-                minScaleX?:number;
-                minScaleY?:number;
-                maxScaleX?:number;
-                maxScaleY?:number;
-                scaleGrid?:number;
-                fittingBBox?:{ x: number; y: number; width: number;height: number}
-            }):void;
+            centerContent(): void;
+
+            zoom(value: number, opt?: {
+                max: number;
+                min: number;
+                grid: any;
+                absolute: boolean;
+            }): void;
+
+            zoomToFit(opt?: {
+                padding?: number;
+                preserveAspectRatio?: boolean;
+                minScaleX?: number;
+                minScaleY?: number;
+                maxScaleX?: number;
+                maxScaleY?: number;
+                scaleGrid?: number;
+                fittingBBox?: { x: number; y: number; width: number;height: number}
+            }): void;
 
 
         }
